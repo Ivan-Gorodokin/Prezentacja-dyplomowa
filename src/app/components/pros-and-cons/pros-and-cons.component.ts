b39492675486df4c4821a342c08c1d683c37a0f4
Ivan Gorodokin <gorodokin98@gmail.com>
2023-06-28 20:12:15 +0200

    Pros and Cons done && arrows icons added

--- src/app/components/pros-and-cons/pros-and-cons.component.ts
+++ src/app/components/pros-and-cons/pros-and-cons.component.ts
@@ -2,12 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavigationService } from 'src/app/services/navigation.service';
 
+export interface TablePair {
+  pro: string;
+  con: string;
+}
+
 @Component({
   selector: 'app-pros-and-cons',
   templateUrl: './pros-and-cons.component.html',
   styleUrls: ['./pros-and-cons.component.scss'],
 })
 export class ProsAndConsComponent implements OnInit {
+  data: TablePair[] = [
+    { pro: `przejrzystość`, con: `brak skalowalności` },
+    { pro: `interaktywność`, con: `ograniczony zakres urządzeń` },
+    {
+      pro: `funkcjonalność`,
+      con: `niewiele dostępnych standardów komunikacji`,
+    },
+    { pro: `poziomy zaawansowania`, con: `mało narzędzi do analizy danych` },
+    { pro: `wsparcie`, con: `` },
+  ];
+
   constructor(
     public navService: NavigationService,
     private route: ActivatedRoute
